4dd5f72548fc3aabc58a23649ec3907127db3656
Jane-547 <Jane-547@bk.ru>
2023-04-12 15:38:10 +0300

    Useful information in Temp

--- Seminars/Temp.py
+++ Seminars/Temp.py
@@ -1,2 +1,20 @@
+for i in range(0, 5, 2):
+print(i)
+
+stroka = "qwerty"
+for el in stroka:
+print(el, end = " ")  # end = " " это чтобы выводилось в строку через пробел, а не в столбец
+
+
+i = 0
+while (i < 5):
+n = int(input("Ввод: "))
+if n == 0:
+break
+i+=1
+else:
+print("Конец")
+
+
 
 
