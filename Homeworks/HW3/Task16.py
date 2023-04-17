dd4ae926a24c278239d7fd70976cdd6c9e6dc070
Jane-547 <Jane-547@bk.ru>
2023-04-17 09:06:18 +0300

    Task # 16 added

---
+++ Homeworks/HW3/Task16.py
@@ -0,0 +1,10 @@
+import random
+
+quantity = int(input('Введите количество элементов массива: '))
+
+array = [random.randint(0, 9) for i in range(quantity)]
+print(array, end = " ")
+
+element = int(input(f'\nВведите число X: '))
+
+print(array.count(element))
