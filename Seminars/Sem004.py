96c4e36f7cdc9eba8e822ddb794d34f4db0cd0fc
Jane-547 <Jane-547@bk.ru>
2023-04-18 15:40:54 +0300

    Sem # 4 added

---
+++ Seminars/Sem004.py
@@ -0,0 +1,34 @@
+letter = input('Введите значения через пробел: ').split()
+# a a a b c a a d c d d
+print(letter)
+
+for i in range(len(letter)):
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea
+# shore shells
+
+letter = input('Введите текст: ').split()
+print(letter)
+
+spisok = set()
+
+for word in letter:
+  spisok.add()
+
+
+
+# for i in range(len(letter)):
+#   if not i.strip():
+#     continue
+
+#   spisok.append(i.lstrip())
+
+#   print(f'Количество уникальных слов в тексте - {len(spisok)}')
+
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------
