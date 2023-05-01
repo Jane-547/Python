7cf8b2967a533e2b8bb079df59a72e017b5f688c
Jane-547 <Jane-547@bk.com>
2023-05-01 18:11:43 +0300

    Task # 30 added

--- Seminars/Temp.py
+++ Seminars/Temp.py
@@ -1,50 +1,65 @@
-for i in range(0, 5, 2):
-print(i)
+# for i in range(0, 5, 2):
+# print(i)
+#
+# stroka = "qwerty"
+# for el in stroka:
+# print(el, end = " ")  # end = " " это чтобы выводилось в строку через пробел, а не в столбец
+#
+#
+# i = 0
+# while (i < 5):
+# n = int(input("Ввод: "))
+# if n == 0:
+# break
+# i+=1
+# else:
+# print("Конец")
+#
+#
+#
+#
+# i = 0
+# while (i < 5):
+# n = int(input("Ввод: "))
+# if n == 0:
+# break
+# i+=1
+# else:
+# print("Конец")
+#
+#
+#
+# slovar = {"1": "one", "2": "one"}
+# print(slovar)
+# slovar["privet"] = "one"
+# print(slovar)
+# # for item in slovar:
+# #     print(item, slovar[item])
+# spisok = []
+# for key, value in slovar.items():
+#     spisok.append([key, value])
+#
+# print(spisok)
+#
+#
+#
+# spisok = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(spisok[5:2:-1])
+#
+# spisok2 = spisok[::-1]
+# print(spisok2)
 
-stroka = "qwerty"
-for el in stroka:
-print(el, end = " ")  # end = " " это чтобы выводилось в строку через пробел, а не в столбец
+import random
+n = int(input("Введите кол-во элементов: "))
+x = int(input("Введите число:  "))
+for i in range(n):
+    a = [random.randint(-10,10)]
+print(*a ,end="  ")
 
-
-i = 0
-while (i < 5):
-n = int(input("Ввод: "))
-if n == 0:
-break
-i+=1
-else:
-print("Конец")
-
-
-
-
-i = 0
-while (i < 5):
-n = int(input("Ввод: "))
-if n == 0:
-break
-i+=1
-else:
-print("Конец")
-
-
-
-slovar = {"1": "one", "2": "one"}
-print(slovar)
-slovar["privet"] = "one"
-print(slovar)
-# for item in slovar:
-#     print(item, slovar[item])
-spisok = []
-for key, value in slovar.items():
-    spisok.append([key, value])
-
-print(spisok)
-
-
-
-spisok = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(spisok[5:2:-1])
-
-spisok2 = spisok[::-1]
-print(spisok2)
+min = max(a) - x
+el = 0
+for i in range(len(a)):
+    if abs(a[i] - x) < min:
+        min = abs(a[i] - x)
+        el = a[i]
+print(el)
